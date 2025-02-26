74f2ae29cf44b6d57572bb8b1f20e8ae94d63f77
Gurusamy-zoho <gurusamysivasankar591@gmail.com>
2025-02-26 10:26:08 +0530

    All files are added

---
+++ script.js
@@ -0,0 +1,26 @@
+let ampm = document.getElementById("DayNight");
+
+function displayTime(){
+
+    let time = new Date();
+
+    let hour = time.getHours();
+    let mins = addZero(time.getMinutes());
+    let secs = addZero(time.getSeconds());
+
+    if(hour>12){
+        hour = hour - 12;
+        ampm.innerHTML = "PM";
+     }
+    
+    document.getElementById("hours").innerHTML = addZero(hour);
+    document.getElementById("mins").innerHTML= mins;
+    document.getElementById("secs").innerHTML = secs;
+     
+}
+
+function addZero(num){
+    return num<10?"0"+num:num;
+}
+
+setInterval(displayTime,100);
